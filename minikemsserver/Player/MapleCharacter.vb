46bdf7f2b806f4d4fe501b0fca39d5aade0ab36d
0minike0 <0minike0@gmail.com>
2011-09-17 15:49:03 +0000

    Added equipstats and made those stats show in game

--- minikemsserver/Player/MapleCharacter.vb
+++ minikemsserver/Player/MapleCharacter.vb
@@ -1,4 +1,19 @@
-﻿Imports MySql.Data.MySqlClient
+﻿'    This file is part of MinikeMSServer.
+
+'    MinikeMSServer is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+
+'    MinikeMSServer is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+
+'    You should have received a copy of the GNU General Public License
+'    along with MinikeMSServer.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports MySql.Data.MySqlClient
 
 Public Class MapleCharacter
     Public id As Integer = 0
